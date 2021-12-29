a6412533b0d17163c4b27d7d84d6b2bfd963b121
Filipe Varjão <varjaofilipe@gmail.com>
2021-12-29 11:55:23 -0300

    first commit

---
+++ lib/ctower.ex
@@ -0,0 +1,18 @@
+defmodule Ctower do
+  @moduledoc """
+  Documentation for `Ctower`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ctower.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ctower_test.exs
@@ -0,0 +1,8 @@
+defmodule CtowerTest do
+  use ExUnit.Case
+  doctest Ctower
+
+  test "greets the world" do
+    assert Ctower.hello() == :world
+  end
+end
